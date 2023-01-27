631419c66b02fe4681b5356fdbb65c291f9dfc5f
Vadim <vadimosipovcng@gmail.com>
2023-01-27 23:20:13 +0200

    add repository

---
+++ Hotels/Data/HotelDb.cs
@@ -0,0 +1,10 @@
+namespace Hotels.Data;
+
+public class HotelDb : DbContext
+{
+    public HotelDb(DbContextOptions<HotelDb> options) : base(options)
+    {
+    }
+
+    public DbSet<Hotel> Hotels => Set<Hotel>();
+}
---
+++ Hotels/Data/IHotelRepository.cs
@@ -0,0 +1,11 @@
+namespace Hotels.Data;
+
+public interface IHotelRepository : IDisposable
+{
+    Task<List<Hotel>> GetHotelsAsync();
+    Task<Hotel?> GetHotelAsync(int hotelId);
+    Task InsertHotelAsync(Hotel hotel);
+    Task UpdateHotelAsync(Hotel hotel);
+    Task DeleteHotelAsync(int hotelId);
+    Task SaveAsync();
+}
